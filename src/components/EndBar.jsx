2f391e94b167c34df0c8b12edc81304bf319795c
Audun Haugen <Audun.Haugen@student.uib.no>
2024-03-13 14:08:37 +0100

    created footer

--- src/components/EndBar.jsx
+++ src/components/EndBar.jsx
@@ -1,9 +1,23 @@
+import './EndBar.css';
 
 const EndBar = () => {
     return (
-        <div>
-            LowerBar Placeholder
-        </div>
+        <>
+            <footer className="footerContainer1">
+                <ul className="NavList">
+                    <li className="NavItem"><a className="NavLink" href="#">ABOUT US</a></li>
+                    <li className="NavItem"><a className="NavLink" href="#">CUSTOMER SERVICE</a></li>
+                    <li className="NavItem"><a className="NavLink" href="#">TRAVELING INFORMATION</a></li>
+                </ul>
+            </footer>
+            <footer className="footerContainer2">
+                <ul className="NavList">
+                    <li className="NavItem"><a className="NavLink" href="#">COOKIES</a></li>
+                    <li className="NavItem"><a className="NavLink" href="#">TERMS OF USE</a></li>
+                    <li className="NavItem"><a className="NavLink" href="#">DATA MANAGING</a></li>
+                </ul>
+            </footer>
+        </>
     );
 }
 
