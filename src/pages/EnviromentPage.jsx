41efe41f2907f9c123823f2badfe9e02a9b0c438
Audun Haugen <Audun.Haugen@student.uib.no>
2024-04-08 17:49:51 +0200

    &apos;

--- src/pages/EnviromentPage.jsx
+++ src/pages/EnviromentPage.jsx
@@ -13,7 +13,7 @@ const EnviromentPage = () => {
                 In a world increasingly aware of the delicate balance of our ecosystem, Kvikk Trip emerges as a beacon for environmentally conscious travelers. 
                 Our app is driven by a profound commitment to preserving the stunning diversity of our planet for future generations. 
                 Recognizing the impact that travel can have on the environment, we aim to empower users  to make informed, sustainable choices. 
-                Whether it's reducing carbon footprints through smarter travel itineraries or promoting eco-friendly accommodations and activities, Kvikk Trip is dedicated to transforming the joy of exploration into a force for environmental stewardship. 
+                Whether it&apos;s reducing carbon footprints through smarter travel itineraries or promoting eco-friendly accommodations and activities, Kvikk Trip is dedicated to transforming the joy of exploration into a force for environmental stewardship. 
                 By leveraging technology, we are not just simplifying travel; we are fostering a global community where each journey enriches both the traveler and the earth.
                 </p>
             </div>
