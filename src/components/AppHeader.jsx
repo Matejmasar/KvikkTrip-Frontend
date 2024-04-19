7a432b5b8adf87041399cd33c9067c14653f53f7
EndreSletnes <endre.sletne@gmail.com>
2024-04-19 21:55:37 +0200

    Added navbar

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,13 +1,23 @@
 import NavBar from "./NavBar.jsx";
+import Icon from '../assets/navbaricon.svg';
 import {useNavigate} from "react-router-dom";
 import './AppHeader.css';
+import {useState} from "react";
 
 const AppHeader = () => {
     const navigator = useNavigate()
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleNavbar = () => {
+        setIsOpen(!isOpen);
+    }
 
     return (
         <div className="appHeader">
-            <NavBar/>
+            <img src={Icon} className="icon" alt="navbar" onClick={toggleNavbar}/>
+            {isOpen && (
+                <NavBar />
+            )}
             {localStorage.getItem('userId') !== null ? (
                 <input type="button" onClick={() => navigator('/userpage')} value="Profile"/>
             ) : (
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,31 @@
+import './NavBar.css';
+import {logoutUser} from "../services/loginservice.js";
 
 const NavBar = () => {
+
+    const handleLogout = async () => {
+        await logoutUser();
+    }
+
     return (
-        <div>
-            Navbar placeholder
-        </div>
+        <li className="navbar">
+            <a href={'/'}>Home</a>
+            {localStorage.getItem('userId') !== null ? (
+                <>
+                    <a href={'/userpage'}>Profile Information</a>
+                    <a href={'/environment'}>Environment Information</a>
+                    <a>Help</a>
+                    <a onClick={handleLogout}>Log out</a>
+                </>
+            ) : (
+                <>
+                    <a href={'/login'}>Log in</a>
+                    <a href={'/register'}>Register</a>
+                    <a href={'/enviroment'}>Environment Information</a>
+                    <a>Help</a>
+                </>
+            )}
+        </li>
     )
 }
 
